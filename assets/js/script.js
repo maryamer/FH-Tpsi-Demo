dcc6442a74e26b8cf21e5bda0a2947a5e27f1ec0
maryam <personalmaryamebrahimi@gmail.com>
2024-06-25 15:33:47 +0330

    complete tap30 mini project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-const toggler = document.querySelector('.nav__toggler')
-const navbar = document.querySelector('.nav')
-toggler?.addEventListener('click',(e)=>{
-navbar.classList.toggle('nav__expanded')
-})
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  console.log("clicked");
+  navbar.classList.toggle("nav__expanded");
+});
+
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+    // REMOVE PREV ACTIVE CLASS
+    tabs.forEach((item) => item.classList.remove("active"));
+    tabContents.forEach((item) => item.classList.remove("active"));
+    // ADD NEW ACTIVE CLASS
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
+});
