fefd2cc91420d2ce9983d6a37c85888b850ca3df
maryam <personalmaryamebrahimi@gmail.com>
2024-06-25 13:58:58 +0330

    mix sections completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const toggler = document.querySelector('.nav__toggler')
 const navbar = document.querySelector('.nav')
-toggler.addEventListener('click',(e)=>{
+toggler?.addEventListener('click',(e)=>{
 navbar.classList.toggle('nav__expanded')
 })
